ca7f0fbfabd487b90b6f0617fc49ef04aa0c72d5
Rafael Machtura <rmachtura@gmail.com>
2022-10-25 20:52:13 -0300

    Finalizando seção 7

--- projeto/settings.py
+++ projeto/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'projeto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            'projeto/base_templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,7 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'recipes/static/'
+STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoprojeto.com/en/4.1/ref/settings/#default-auto-field
